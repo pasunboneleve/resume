fa6e76591e7c72906a82f11137bbb6598d098118
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2026-05-19 15:48:42 +1000

    update rename with hybrid.typ

--- rename.sh
+++ rename.sh
@@ -1,2 +1,3 @@
 typst compile general.typ  "Vianna - $(date +%F) - Systems Translator - Resume.pdf"
 typst compile boring.typ  "Vianna - $(date +%F) - Software Engineer - Resume.pdf"
+typst compile hybrid.typ  "Vianna - $(date +%F) - Software and Data Engineer - Resume.pdf"
