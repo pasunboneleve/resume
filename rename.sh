090bfb37115d43c08c311bcc883abb4e15f4169d
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2026-05-19 11:23:00 +1000

    rename general

--- rename.sh
+++ rename.sh
@@ -1,2 +1,2 @@
-typst compile general.typ  "Vianna - $(date +%F) - Resume.pdf"
+typst compile general.typ  "Vianna - $(date +%F) - Systems Translator - Resume.pdf"
 typst compile boring.typ  "Vianna - $(date +%F) - Software Engineer - Resume.pdf"
